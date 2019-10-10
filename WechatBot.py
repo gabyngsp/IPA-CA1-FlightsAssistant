22bae3e44625d9ce1a423c076a14ac35a66af7ae
Yifei <gyf1014@gmail.com>
2019-10-10 12:20:09 +0800

    modify parameter's name

--- WechatBot.py
+++ WechatBot.py
@@ -1,13 +1,16 @@
-import time, datetime, requests, itchat
+import itchat
+import tagui as t
 from itchat.content import *
 #itchat.auto_login()
 itchat.auto_login(hotReload=True)
 
-username = itchat.search_friends(name=u'伦家小Yanni')[0]['UserName']
-itchat.send_file('1.xlsx',toUserName=username)
+#username = itchat.search_friends(name=u'伦家小Yanni')[0]['UserName']
+#itchat.send_file('1.xlsx',toUserName=username)
+t.init()
+t.url('https://www.skyscanner.com.sg/')
 
-info = {"depart":"","dest":"","start_date":"","end_date":"","type":0}
-enquiry = [False,False,False,False,False] # depart ,dest, start date, end date, type
+info = {"from":"","to":"","start_date":"","end_date":"","trip_type":""}
+enquiry = [False,False,False,False,False]  # depart ,dest, start date, end date, type
 flag = False
 
 
@@ -18,34 +21,34 @@ def book_flight(msg):
 
     if "book flights" in msg['Text']:
         flag = True
-        itchat.send('Your departure city.')
+        itchat.send('Your departure city.', msg['FromUserName'])
         return info
     elif not enquiry[0] and flag:
-        info["depart"] = msg['Text']
+        info["from"] = msg['Text']
         enquiry[0] = True
-        itchat.send('Your destination city.')
+        itchat.send('Your destination city.', msg['FromUserName'])
     elif not enquiry[1] and flag:
-        info["dest"] = msg['Text']
+        info["to"] = msg['Text']
         enquiry[1] = True
-        itchat.send('Your start date.')
+        itchat.send('Your start date.', msg['FromUserName'])
     elif not enquiry[2] and flag:
         info["start_date"] = msg['Text']
         enquiry[2] = True
-        itchat.send('Your end date.')
+        itchat.send('Your end date.', msg['FromUserName'])
     elif not enquiry[3] and flag:
         info["end_date"] = msg['Text']
         enquiry[3] = True
-        itchat.send('Your ticket type\n 1.One way\n 2.Return.')
+        itchat.send('Your ticket type\n 1.One way\n 2.Return\n 3.Multi-City.', msg['FromUserName'])
     elif not enquiry[4] and flag:
-        info["type"] = int(msg['Text'])
+        info["trip_type"] = msg['Text']
         enquiry[4] = True
     if all(enquiry):
         flag = False
         result = info
-        info = {"depart": "", "dest": "", "start_date": "", "end_date": "", "type": 0}
+        info = {"from": "", "to": "", "start_date": "", "end_date": "", "trip_type": ""}
         enquiry = [False, False, False, False, False]
-        itchat.send('Please wait for the result')
-        # fun(result)
+        itchat.send('Please wait for the result', msg['FromUserName'])
+        print(result)
 
 itchat.run()
-itchat.logout()
+#itchat.logout()
